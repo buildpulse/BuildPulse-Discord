cf90d1f5c81289678555860474a5561958f61d4b
benjadahl <benjadahl@gmail.com>
2022-01-13 17:12:17 +0100

    Add proper error msg

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const axios = require('axios');
 const core = require('@actions/core');
 
-(async () => {
-  const bpData = (await axios(
-    {
-      url: 'https://buildpulse.io/api/repos/rabatta-aps/rabatta/tests',
-      headers: {
-        Authorization: `token ${core.getInput('buildPulse-api-token')}`
+try {
+  (async () => {
+    const bpData = (await axios(
+      {
+        url: 'https://buildpulse.io/api/repos/rabatta-aps/rabatta/tests',
+        headers: {
+          Authorization: `token ${core.getInput('buildPulse-api-token')}`
+        }
       }
+    )).data;
+  
+    const tests = bpData.tests;
+  
+    tests.sort((a, b) => {
+      return b.disruptiveness - a.disruptiveness;
+    });
+  
+    let content = '__**The most flaky tests the last 14 days**__\n```';
+  
+    for (let i = 0; i < 3 && i < tests.length; i++) {
+      const test = tests[i];
+      content += `Disruptiveness: ${test.disruptiveness} - ${test.name}\n`;
     }
-  )).data;
-
-  const tests = bpData.tests;
-
-  tests.sort((a, b) => {
-    return b.disruptiveness - a.disruptiveness;
-  });
-
-  let content = '__**The most flaky tests the last 14 days**__\n```';
-
-  for (let i = 0; i < 3 && i < tests.length; i++) {
-    const test = tests[i];
-    content += `Disruptiveness: ${test.disruptiveness} - ${test.name}\n`;
-  }
-
-  content += '```';
-
-  axios({
-    method: 'POST',
-    url: core.getInput('discord-webhook'),
-    data: {
-      content: content
-    }
-  });
-})();
+  
+    content += '```';
+  
+    axios({
+      method: 'POST',
+      url: core.getInput('discord-webhook'),
+      data: {
+        content: content
+      }
+    });
+  })();
+} catch (e) {
+  core.setFailed(e);
+}
