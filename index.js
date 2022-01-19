f531c318b754f8479d878d91483a6ba3a214e839
benjadahl <benjadahl@gmail.com>
2022-01-19 16:49:15 +0100

    Set error messages for fetching

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const axios = require('axios');
 const core = require('@actions/core');
 
-try {
   (async () => {
     const bpToken = core.getInput('buildPulse-api-token');
     core.debug(bpToken);
-    if (!bpToken) core.setFailed('Missing BuildPulse API token');
+    //if (!bpToken) core.setFailed('Missing BuildPulse API token');
     const discordWebhook = core.getInput('discord-webhook');
     core.debug(discordWebhook);
-    if (!discordWebhook) core.setFailed('Missing Discord webhook');
+    //if (!discordWebhook) core.setFailed('Missing Discord webhook');
     const repo = core.getInput('repository') || process.env.GITHUB_REPOSITORY;
-    /*const bpData = (await axios(
-      {
-        url: `https://buildpulse.io/api/repos/${repo}/tests`,
-        headers: {
-          Authorization: `token ${bpToken}`
+    let bpData;
+    try {
+      bpData = (await axios(
+        {
+          url: `https://buildpulse.io/api/repos/${repo}/tests`,
+          headers: {
+            Authorization: `token ${bpToken}`
+          }
         }
-      }
-    )).data;
+      )).data;
+    } catch (e) {
+      core.setFailed(`Failed to communicate with BuildPulse: ${e}`);
+    }
   
     const tests = bpData.tests;
   
@@ -38,14 +42,16 @@ try {
   
     content += '```';
   
-    axios({
-      method: 'POST',
-      url: discordWebhook,
-      data: {
-        content: content
-      }
-    });*/
+    try {
+      axios({
+        method: 'POST',
+        url: discordWebhook,
+        data: {
+          content: content
+        }
+      });
+    } catch (e) {
+      core.setFailed(`Failed to communicate with Discord: ${e}`);
+    }
   })();
-} catch (e) {
-  core.setFailed(e);
-}
+
